5c21d6b8481785035216c0f97d2a15b2b59cb806
niklas-note <niklaslinz@gmx.de>
2016-05-30 14:28:46 +0200

    Update config part 2

--- src/main/java/de/kekshaus/cubit/land/plugin/LandConfig.java
+++ src/main/java/de/kekshaus/cubit/land/plugin/LandConfig.java
@@ -1,5 +1,6 @@
 package de.kekshaus.cubit.land.plugin;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import de.kekshaus.cubit.land.Landplugin;
@@ -9,6 +10,20 @@ public class LandConfig {
 	private Landplugin plugin;
 	private FileConfiguration configFile;
 
+	/* Public config values */
+
+	/* Land module */
+	public double landBasePrice;
+	public double landTaxAddition;
+	public double landSellPercent;
+	public double landDeprecatedMember;
+	public double landDeprecatedOther;
+	public boolean landUseMaterialBorder;
+	public Material landBuyMaterialBorder;
+	public Material landSellaterialBorder;
+
+	/* Database module */
+	public boolean sqlUse;
 	public String sqlDataBase;
 	public String sqlHostname;
 	public int sqlPort;
@@ -39,15 +54,34 @@ public class LandConfig {
 	}
 
 	private void setContent() {
-		checkContent("path", 333306);
+
+		/* Land module */
+		checkContent("module.land.basePrice", 235.00D);
+		checkContent("module.land.taxAddition", 5.00D);
+		checkContent("module.land.sellPercentInDecimal", 0.5D);
+		checkContent("module.land.deprecatedBuyupMember", 30.0D);
+		checkContent("module.land.deprecatedBuyupOther", 45.00D);
+		checkContent("module.land.useMaterialBorder", true);
+		checkContent("module.land.buyMaterialBorder", Material.TORCH);
+		checkContent("module.land.sellMaterialBorder", Material.REDSTONE_TORCH_ON);
+
+		/* Database module */
+		checkContent("module.database.useSql", false);
+		checkContent("module.database.databaseName", "cubit");
+		checkContent("module.database.hostName", "localhost");
+		checkContent("module.database.port", 3306);
+		checkContent("module.database.userName", "yourSqlUser");
+		checkContent("module.database.password", "yourSqlPassword");
+
 	}
 
 	private void loadContent() {
-		this.sqlDataBase = (String) this.configFile.get("path");
-		this.sqlHostname = (String) this.configFile.get("path");
-		this.sqlPort = (int) this.configFile.get("path");
-		this.sqlUser = (String) this.configFile.get("path");
-		this.sqlPassword = (String) this.configFile.get("path");
+		this.sqlUse = (boolean) this.configFile.get("module.database.useSql");
+		this.sqlDataBase = (String) this.configFile.get("module.database.databaseName");
+		this.sqlHostname = (String) this.configFile.get("module.database.hostName");
+		this.sqlPort = (int) this.configFile.get("module.database.port");
+		this.sqlUser = (String) this.configFile.get("module.database.userName");
+		this.sqlPassword = (String) this.configFile.get("module.database.password");
 	}
 
 	private void checkContent(String path, Object defaultValue) {
