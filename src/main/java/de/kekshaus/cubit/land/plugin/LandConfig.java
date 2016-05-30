dece5feae8c42b8f8d39b3973ed011dd1d9d11f2
niklas-note <niklaslinz@gmx.de>
2016-05-30 11:23:54 +0200

    Init config part 1

--- src/main/java/de/kekshaus/cubit/land/plugin/LandConfig.java
+++ src/main/java/de/kekshaus/cubit/land/plugin/LandConfig.java
@@ -1,10 +1,14 @@
 package de.kekshaus.cubit.land.plugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 import de.kekshaus.cubit.land.Landplugin;
 
 public class LandConfig {
 
 	private Landplugin plugin;
+	private FileConfiguration configFile;
+
 	public String sqlDataBase;
 	public String sqlHostname;
 	public int sqlPort;
@@ -13,6 +17,54 @@ public class LandConfig {
 
 	public LandConfig(Landplugin plugin) {
 		this.plugin = plugin;
+		initConfig();
+	}
+
+	private void initConfig() {
+		saveConfig();
+		initContent();
+	}
+
+	private void saveConfig() {
+		this.plugin.saveDefaultConfig();
+		this.plugin.saveConfig();
+		this.plugin.reloadConfig();
+		this.configFile = this.plugin.getConfig();
+	}
+
+	private void initContent() {
+		setContent();
+		saveConfig();
+		loadContent();
+	}
+
+	private void setContent() {
+		checkContent("path", 333306);
+	}
+
+	private void loadContent() {
+		this.sqlDataBase = (String) this.configFile.get("path");
+		this.sqlHostname = (String) this.configFile.get("path");
+		this.sqlPort = (int) this.configFile.get("path");
+		this.sqlUser = (String) this.configFile.get("path");
+		this.sqlPassword = (String) this.configFile.get("path");
+	}
+
+	private void checkContent(String path, Object defaultValue) {
+		if (!containsContent(path)) {
+			addContent(path, defaultValue);
+		}
+	}
+
+	private void addContent(String path, Object defaultValue) {
+		this.configFile.set(path, defaultValue);
+	}
+
+	private boolean containsContent(String path) {
+		if (this.configFile.contains(path)) {
+			return true;
+		}
+		return false;
 	}
 
 }
