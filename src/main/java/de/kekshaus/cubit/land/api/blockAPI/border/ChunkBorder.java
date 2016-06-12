c9d3ebb2694179dc2dc380d5bdd3ccec41fa0264
Niklas Note <niklaslinz@hotmail.com>
2016-06-12 10:12:59 +0200

    Try to start block-calculater in async Task

--- src/main/java/de/kekshaus/cubit/land/api/blockAPI/border/ChunkBorder.java
+++ src/main/java/de/kekshaus/cubit/land/api/blockAPI/border/ChunkBorder.java
@@ -6,69 +6,57 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
-/**
- * Copyright:
- * <ul>
- * <li>Autor: Kekshaus</li>
- * <li>2016</li>
- * <li>www.minegaming.de</li>
- * </ul>
- * 
- */
-
 public class ChunkBorder implements Runnable {
 
 	private Plugin plugin;
 	private Chunk chunk;
 	private Material torchMaterial;
 
-	private TorchPlaceWork work;
+	private Worker work;
 
 	public ChunkBorder(Plugin plugin, Chunk chunk, Material torchMaterial) {
 		this.plugin = plugin;
 		this.chunk = chunk;
 		this.torchMaterial = torchMaterial;
-		work = new TorchPlaceWork();
-		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, this);
+		work = new Worker();
+		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, this);
 	}
 
 	@Override
 	public void run() {
-		if (work.isWorkDone()) {
+		if (work.isTaskComplete()) {
 			return;
 		}
 
-		for (int count = 0; count < 4 && !work.isWorkDone(); work.i++, count++) {
-			Location block1 = chunk.getBlock(work.i, 0, 0).getLocation();
-			block1.setY(getY(chunk.getWorld(), block1.getBlockX(), block1.getBlockZ()));
-			chunk.getWorld().getBlockAt(block1).setType(torchMaterial);
+		for (int count = 0; count < 4 && !work.isTaskComplete(); work.i++, count++) {
+			Location corner_1 = chunk.getBlock(work.i, 0, 0).getLocation();
+			corner_1.setY(getValidBlockHight(chunk.getWorld(), corner_1.getBlockX(), corner_1.getBlockZ()));
 
-			Location block2 = chunk.getBlock(15, 0, work.i).getLocation();
-			block2.setY(getY(chunk.getWorld(), block2.getBlockX(), block2.getBlockZ()));
-			chunk.getWorld().getBlockAt(block2).setType(torchMaterial);
+			Location corner_2 = chunk.getBlock(15, 0, work.i).getLocation();
+			corner_2.setY(getValidBlockHight(chunk.getWorld(), corner_2.getBlockX(), corner_2.getBlockZ()));
 
-			Location block3 = chunk.getBlock((15 - work.i), 0, 15).getLocation();
-			block3.setY(getY(chunk.getWorld(), block3.getBlockX(), block3.getBlockZ()));
-			chunk.getWorld().getBlockAt(block3).setType(torchMaterial);
+			Location corner_3 = chunk.getBlock((15 - work.i), 0, 15).getLocation();
+			corner_3.setY(getValidBlockHight(chunk.getWorld(), corner_3.getBlockX(), corner_3.getBlockZ()));
 
-			Location block4 = chunk.getBlock(0, 0, (15 - work.i)).getLocation();
-			block4.setY(getY(chunk.getWorld(), block4.getBlockX(), block4.getBlockZ()));
-			chunk.getWorld().getBlockAt(block4).setType(torchMaterial);
+			Location corner_4 = chunk.getBlock(0, 0, (15 - work.i)).getLocation();
+			corner_4.setY(getValidBlockHight(chunk.getWorld(), corner_4.getBlockX(), corner_4.getBlockZ()));
+
+			setBlockSync(torchMaterial, corner_1, corner_2, corner_3, corner_4);
 		}
-		if (!work.isWorkDone()) {
-			plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, this);
+		if (!work.isTaskComplete()) {
+			plugin.getServer().getScheduler().runTaskAsynchronously(plugin, this);
 		}
 	}
 
-	public class TorchPlaceWork {
+	public class Worker {
 		public int i = 0;
 
-		public boolean isWorkDone() {
+		public boolean isTaskComplete() {
 			return i == 15;
 		}
 	}
 
-	public int getY(World world, int x, int z) {
+	public int getValidBlockHight(World world, int x, int z) {
 		int y;
 		for (y = 255; y > 0; y--) {
 			if ((world.getBlockAt(x, y, z).getType() != Material.AIR)
@@ -86,5 +74,20 @@ public class ChunkBorder implements Runnable {
 			}
 		}
 		return y + 1;
+	}
+
+	private void setBlockSync(final Material material, final Location corner_1, final Location corner_2,
+			final Location corner_3, final Location corner_4) {
+		plugin.getServer().getScheduler().runTask(plugin, new Runnable() {
+
+			@Override
+			public void run() {
+				chunk.getWorld().getBlockAt(corner_1).setType(material);
+				chunk.getWorld().getBlockAt(corner_2).setType(material);
+				chunk.getWorld().getBlockAt(corner_3).setType(material);
+				chunk.getWorld().getBlockAt(corner_4).setType(material);
+			}
+
+		});
 	}
 }
