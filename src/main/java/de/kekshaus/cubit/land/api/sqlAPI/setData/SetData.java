02c71330c1c1e6f44ea9f6e1458b6fdced78fa3c
Niklas Note <niklaslinz@hotmail.com>
2016-06-12 19:56:20 +0200

    Fix 4

--- src/main/java/de/kekshaus/cubit/land/api/sqlAPI/setData/SetData.java
+++ src/main/java/de/kekshaus/cubit/land/api/sqlAPI/setData/SetData.java
@@ -16,18 +16,18 @@ public class SetData {
 		try {
 			Connection conn = manager.getConnection("cookieLand");
 			PreparedStatement sql = conn.prepareStatement("SELECT value FROM offerManager WHERE regionID = '"
-					+ data.getRegionID() + "' AND world = '" + data.getWorld() + "';");
+					+ data.getRegionID() + "' AND world = '" + data.getWorld().getName().toLowerCase() + "';");
 			ResultSet result = sql.executeQuery();
 			if (result.next()) {
 				PreparedStatement update = conn.prepareStatement("UPDATE offerManager SET value = '" + data.getValue()
 						+ "', uuid = '" + data.getPlayerUUID().toString() + "' WHERE regionID = '" + data.getRegionID()
-						+ "' AND world = '" + data.getWorld() + "';");
+						+ "' AND world = '" + data.getWorld().getName().toLowerCase() + "';");
 				update.executeUpdate();
 				update.close();
 			} else {
-				PreparedStatement insert = conn
-						.prepareStatement("INSERT INTO offerManager (regionID, value, world, uuid) VALUES ('"
-								+ data.getRegionID() + "', '" + data.getValue() + "', '" + data.getWorld() + "', '"
+				PreparedStatement insert = conn.prepareStatement(
+						"INSERT INTO offerManager (regionID, value, world, uuid) VALUES ('" + data.getRegionID()
+								+ "', '" + data.getValue() + "', '" + data.getWorld().getName().toLowerCase() + "', '"
 								+ data.getPlayerUUID().toString() + "');");
 				insert.executeUpdate();
 				insert.close();
