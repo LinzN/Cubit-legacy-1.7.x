48b4ed5c8e87840be2635b53c24530f10ccc020f
Niklas Note <niklaslinz@hotmail.com>
2016-06-14 11:21:27 +0200

    Update so cubit

--- src/main/java/de/kekshaus/cubit/land/plugin/PermissionNodes.java
+++ src/main/java/de/kekshaus/cubit/land/plugin/PermissionNodes.java
@@ -3,20 +3,20 @@ package de.kekshaus.cubit.land.plugin;
 public class PermissionNodes {
 
 	/* User Perms */
-	public String infoLand = "cubitAlpha.land.info";
-	public String buyLand = "cubitAlpha.land.buy";
-	public String sellLand = "cubitAlpha.land.sell";
-	public String helpLand = "cubitAlpha.land.help";
-	public String addMemberLand = "cubitAlpha.land.addmember";
-	public String removeMemberLand = "cubitAlpha.land.removemember";
-	public String flagLand = "cubitAlpha.land.flag.";
-	public String offerLand = "cubitAlpha.land.offer";
-	public String takeOfferLand = "cubitAlpha.land.takeoffer";
+	public String infoLand = "cubit.command.land.info";
+	public String buyLand = "cubit.command.land.buy";
+	public String sellLand = "cubit.command.land.sell";
+	public String helpLand = "cubit.command.land.help";
+	public String addMemberLand = "cubit.command.land.addmember";
+	public String removeMemberLand = "cubit.command.land.removemember";
+	public String flagLand = "cubit.command.land.flag.";
+	public String offerLand = "cubit.command.land.offer";
+	public String takeOfferLand = "cubit.command.land.takeoffer";
 
 	/* Admin Perms */
-	public String flagAdminLand = "cubitAlpha.admin.flag.";
-	public String sellAdminLand = "cubitAlpha.admin.sell";
-	public String addMemberAdminLand = "cubitAlpha.admin.addmember";
-	public String removeMemberAdminLand = "cubitAlpha.admin.removemember";
+	public String flagAdminLand = "cubit.command.admin.flag.";
+	public String sellAdminLand = "cubit.command.admin.sell";
+	public String addMemberAdminLand = "cubit.command.admin.addmember";
+	public String removeMemberAdminLand = "cubit.command.admin.removemember";
 
 }
