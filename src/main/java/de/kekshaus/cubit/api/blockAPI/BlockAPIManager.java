d4b3dc29abfeb8810a9d8c1a9bf1ca2874cc1726
nikla <nikla@DESKTOP-BI5OPBA>
2016-11-14 19:48:07 +0100

    Add Biome function. But not commands

--- src/main/java/de/kekshaus/cubit/api/blockAPI/BlockAPIManager.java
+++ src/main/java/de/kekshaus/cubit/api/blockAPI/BlockAPIManager.java
@@ -2,7 +2,9 @@ package de.kekshaus.cubit.api.blockAPI;
 
 import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.block.Biome;
 
+import de.kekshaus.cubit.api.blockAPI.biome.ChangeBiome;
 import de.kekshaus.cubit.api.blockAPI.border.ChunkBorder;
 import de.kekshaus.cubit.plugin.Landplugin;
 
@@ -17,6 +19,17 @@ public class BlockAPIManager {
 	public boolean placeLandBorder(Chunk chunk, Material material) {
 		try {
 			new ChunkBorder(plugin, chunk, material);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean changeBiomeChunk(Chunk chunk, Biome biome){
+		
+		try {
+			new ChangeBiome(plugin, chunk, biome);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
---
+++ src/main/java/de/kekshaus/cubit/api/blockAPI/biome/ChangeBiome.java
@@ -0,0 +1,48 @@
+package de.kekshaus.cubit.api.blockAPI.biome;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.plugin.Plugin;
+
+public class ChangeBiome {
+
+	private Plugin plugin;
+	private Chunk chunk;
+	private Biome biome;
+
+	public ChangeBiome(Plugin plugin, Chunk chunk, Biome biome) {
+		this.plugin = plugin;
+		this.chunk = chunk;
+		this.biome = biome;
+	}
+
+	public void change() {
+		final int chunkX = this.chunk.getX() * 16;
+		final int cZ = this.chunk.getZ() * 16;
+		final World world = this.chunk.getWorld();
+
+		for (int x = 0; x < 16; x++) {
+			
+			final int cordX = x + chunkX;
+			
+			Bukkit.getScheduler().runTask(this.plugin, new Runnable() {
+				public void run() {
+					for (int z = 0; z < 16; z++) {
+						world.setBiome(cordX, cZ + z, biome);
+					}
+				}
+
+			});
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
