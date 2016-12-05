0fd2c3e26d10f2924684d2cf8c5187d41bb6cd99
nikla <nikla@DESKTOP-PG1QM40>
2016-12-05 08:58:34 +0100

    Try to refresh chunk on no nms-code.

--- src/main/java/de/kekshaus/cubit/api/blockAPI/nmsPackets/NMS_v0_R1.java
+++ src/main/java/de/kekshaus/cubit/api/blockAPI/nmsPackets/NMS_v0_R1.java
@@ -7,16 +7,20 @@ import de.kekshaus.cubit.api.classes.interfaces.INMSMask;
 
 public class NMS_v0_R1 implements INMSMask {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void refreshChunk(Chunk chunk) {
 		// TODO Auto-generated method stub
+		chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
 
 	}
 
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void sendTitle(Player paramPlayer, String paramString) {
 		// TODO Auto-generated method stub
+		paramPlayer.sendTitle(paramString, "");
 
 	}
 
