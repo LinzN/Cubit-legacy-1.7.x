18ffd6c13bd88cc3de775aed6856e51131735107
Niklas <Niklaslinz@hotmail.com>
2017-12-30 16:52:23 +0100

    Fix Login on disabled spiget updater

--- src/main/java/de/linzn/cubit/bukkit/plugin/listener/LoginListener.java
+++ src/main/java/de/linzn/cubit/bukkit/plugin/listener/LoginListener.java
@@ -1,37 +1,29 @@
 package de.linzn.cubit.bukkit.plugin.listener;
 
-import java.util.Date;
-
+import de.linzn.cubit.bukkit.plugin.CubitBukkitPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
-import de.linzn.cubit.bukkit.plugin.CubitBukkitPlugin;
+import java.util.Date;
 
 public class LoginListener implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(final PlayerLoginEvent event) {
-		Bukkit.getScheduler().runTaskAsynchronously(CubitBukkitPlugin.inst(), new Runnable() {
-
-			@Override
-			public void run() {
-
-				CubitBukkitPlugin.inst().getDataAccessManager().databaseType.set_update_profile(
-						event.getPlayer().getUniqueId(), event.getPlayer().getName(), new Date().getTime());
-				if (event.getPlayer().hasPermission(CubitBukkitPlugin.inst().getPermNodes().checkUpdateAdmin)) {
-					if (CubitBukkitPlugin.inst().getSpigetUpdateCheck().isAvailable) {
-						event.getPlayer()
-								.sendMessage(ChatColor.GREEN
-										+ "A new update for Cubit is avaiable. Check out the new version "
-										+ CubitBukkitPlugin.inst().getSpigetUpdateCheck().version + "!");
-					}
-				}
-
-			}
-
-		});
-	}
+        Bukkit.getScheduler().runTaskAsynchronously(CubitBukkitPlugin.inst(), () -> {
+            CubitBukkitPlugin.inst().getDataAccessManager().databaseType.set_update_profile(
+                    event.getPlayer().getUniqueId(), event.getPlayer().getName(), new Date().getTime());
+            if (event.getPlayer().hasPermission(CubitBukkitPlugin.inst().getPermNodes().checkUpdateAdmin) && CubitBukkitPlugin.inst().getSpigetUpdateCheck() != null) {
+                if (CubitBukkitPlugin.inst().getSpigetUpdateCheck().isAvailable) {
+                    event.getPlayer()
+                            .sendMessage(ChatColor.GREEN
+                                    + "A new update for Cubit is avaiable. Check out the new version "
+                                    + CubitBukkitPlugin.inst().getSpigetUpdateCheck().version + "!");
+                }
+            }
+        });
+    }
 }
